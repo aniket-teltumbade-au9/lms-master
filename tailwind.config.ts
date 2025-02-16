650e25b73b3df4f44483417c73cecfe33264781d
Aniket Teltumbade <teltumbade.s.aniket@gmail.com>
2025-02-16 20:23:48 +0530

    fix: landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,21 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        sm: "300px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
+      colors: {
+        cta: "#E5E7EB", // CTA buttons
+        subtitle: "#000000", // Section & Card subtitles
+        header: "#F59E0B", // Section & Card headers
+        body: "#1E3A8A", // Cards & Body background
+      },
+      // fontFamily: {
+      //   sans: ["Lufga", "sans-serif"], // Custom global font
+      // },
     },
   },
   plugins: [],
