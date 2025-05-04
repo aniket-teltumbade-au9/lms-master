c410e5545c745f0b6caf54addcb85a2c21de984f
Aniket Teltumbade <teltumbade.s.aniket@gmail.com>
2025-05-04 14:43:08 +0530

    feat: added auth and mongodb

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    ignoreDuringBuilds: false, // Set to true if you want to ignore ESLint errors during production builds
+    ignoreDuringBuilds: true, // Set to true if you want to ignore ESLint errors during production builds
   },
   images: {
     domains: ["res.cloudinary.com"], // Allow Cloudinary images
