f42a25981f7b37dd669e86cf0b627e0e267ecbb2
Aniket Teltumbade <teltumbade.s.aniket@gmail.com>
2025-05-24 19:42:16 +0530

    auth options fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,64 +1,6 @@
-// /app/api/auth/[...nextauth]/route.ts
-
-import { connectDB } from "@/lib/mongoose";
-import { UserModel } from "@/models/user.model";
-import bcrypt from "bcrypt";
-import type { AuthOptions } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: AuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "email" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) return null;
-
-                await connectDB();
-                const user = await UserModel.findOne({ email: credentials.email });
-                if (!user) return null;
-
-                const isValid = await bcrypt.compare(credentials.password, user.password);
-                if (!isValid) return null;
-
-                return {
-                    id: user._id.toString(),
-                    ...user._doc,
-                };
-            }
-        })
-    ],
-    session: {
-        strategy: "jwt" as const,
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async jwt({ token, user }) {
-            // ✅ Add user info to the token at login
-            if (user) {
-                token.id = user.id;
-                token.email = user.email;
-                token.name = user.name;
-                token.role = user.role;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            // ✅ Make token info available in session
-            if (token) {
-                session.user.id = token.id as string;
-                session.user.email = token.email as string;
-                session.user.name = token.name as string;
-                session.user.role = token.role as string;
-            }
-            return session;
-        },
-    }
-};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
